b3224c6444c381f45f01c86408bc9baa326a6e9c
Lil <liliapaper@gmail.com>
2022-10-06 20:39:51 +0300

    First test version

---
+++ src/test/java/liliapaper/com/SelenideFileTest.java
@@ -0,0 +1,67 @@
+package liliapaper.com;
+
+import com.codeborne.selenide.Configuration;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.InflaterInputStream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SelenideFileTest {
+
+    @BeforeAll
+    public static void setUp() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+
+    //the most common way - from webelement with link "href"
+    @Test
+    public void fileFromHrefWithTryFinallyTest() throws Exception {
+        open("http://www.24copy.ru/skachat-prajjs.html");
+        File file = $("a[title='Скачать прайс-лист в формате Microsoft Excel']").download();
+        InputStream is = new FileInputStream(file);
+        try{
+        byte[] filesource = is.readAllBytes();
+        String filecontent = new String(filesource, StandardCharsets.UTF_8);
+        assertThat(filecontent).contains("OOO Строймаркет");
+    }finally {
+            is.close();
+        }
+    }
+
+    @Test
+    public void fileFromHrefWithTryWithResourcesTest() throws Exception {
+        open("http://www.24copy.ru/skachat-prajjs.html");
+        File file = $("a[title='Скачать прайс-лист в формате Microsoft Excel']").download();
+        try(InputStream is = new FileInputStream(file)){
+            byte[] filesource = is.readAllBytes();
+            String filecontent = new String(filesource, StandardCharsets.UTF_8);
+            assertThat(filecontent).contains("OOO Строймаркет");
+        }
+    }
+
+    @Test
+    public void fileFromHrefWithoutTryCatchTest() throws Exception {
+        open("https://github.com/hoppscotch/hoppscotch");
+        final File file = $("a.Link--primary[href='#readme']").download();
+        String contents = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        assertThat(contents).contains("Customizable combinations for background, foreground, and accent colors");
+        }
+
+        //download file by button
+    @Test
+    public void fileFromButton(){
+        open("https://fineuploader.com/demos.html");
+        File file = $("input[type='file']").uploadFromClasspath("матрица.png");
+        $(".qq-file-info").shouldHave(text("матрица.png"));
+        $(".qq-upload-size-selector.qq-upload-size").shouldHave(text("159.2kB"));
+    }
+}
---
+++ src/test/java/liliapaper/com/ZipFileTest.java
@@ -0,0 +1,60 @@
+package liliapaper.com;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ZipFileTest {
+
+    ClassLoader cl = ZipFileTest.class.getClassLoader();
+
+    @Test
+    public void ZipTest() throws Exception {
+        InputStream is = cl.getResourceAsStream("peoples.zip");
+        ZipFile zipfile = new ZipFile("src/test/resources/peoples.zip");
+        ZipInputStream zis = new ZipInputStream(is);
+        ZipEntry entry;
+        while ((entry = zis.getNextEntry()) != null) {
+            String entryName = entry.getName();
+            if (entryName.contains("csv")) {
+                try (InputStream in = zipfile.getInputStream(entry)) {
+                    CSVReader reader = new CSVReader(new InputStreamReader(in));
+                    List<String[]> content = reader.readAll();
+                    String[] row = content.get(0);
+                    String[] rowb = content.get(1);
+                    String[] rowc = content.get(2);
+                    assertThat(row[0]).isEqualTo("name; surname; company;");
+                    assertThat(rowb[0]).isEqualTo("Albert; Trust; Amazon;");
+                    assertThat(rowc[0]).isEqualTo("Helen; Olive; Gazprom;");
+                }
+            }
+            if (entryName.contains("pdf")) {
+                try (InputStream in = zipfile.getInputStream(entry)) {
+                    PDF pdf = new PDF(in);
+                    assertThat(pdf.numberOfPages).isEqualTo(23);
+                }
+            }
+            if (entryName.contains("xlsx")) {
+                try (InputStream in = zipfile.getInputStream(entry)) {
+                    XLS xls = new XLS(in);
+                    assertThat(xls.excel.getSheetAt(0)
+                            .getRow(50)
+                            .getCell(1)
+                            .getStringCellValue())
+                            .isEqualTo("Rasheeda");
+                }
+            }
+        }
+    }
+}
+
